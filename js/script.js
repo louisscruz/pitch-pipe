edc6996ba8f201ccb973c633be3b52d7a7de6798
louisscruz <louisstephancruz@me.com>
2014-06-06 18:45:28 -0400

    Fix <b>s in output

--- js/script.js
+++ js/script.js
@@ -15,34 +15,24 @@ $(function($) {
           knobValue = value;
           if(value==1) {
             $("#output").html("<h2>C<b>&#x266f;</b></h2>");
-            $("#output b")
-              
           } else if(value==2) {
             $("#output").html("<h2>D</h2>");
           } else if(value==3) {
             $("#output").html("<h2>D<b>&#x266f;</b></h2>");
-            $("#output b")
-              .css( "font-size", "100px" );
           } else if(value==4) {
             $("#output").html("<h2>E</h2>");
           } else if(value==5) {
             $("#output").html("<h2>F</h2>");
           } else if(value==6) {
             $("#output").html("<h2>F<b>&#x266f;</b></h2>");
-            $("#output b")
-              .css( "font-size", "100px" );
           } else if(value==7) {
             $("#output").html("<h2>G</h2>");
           } else if(value==8) {
             $("#output").html("<h2>G<b>&#x266f;</b></h2>");
-            $("#output b")
-              .css( "font-size", "100px" );
           } else if(value==9) {
             $("#output").html("<h2>A</h2>");
           } else if(value==10) {
             $("#output").html("<h2>B<b>&#x266d;</b></h2>");
-            $("#output b")
-              .css( "font-size", "100px" );
           } else if(value==11) {
             $("#output").html("<h2>B</h2>");
           } else {
