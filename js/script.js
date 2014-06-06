81ce9ce6a80bdbe52a62dd425550144f695b4d93
louisscruz <louisstephancruz@me.com>
2014-06-06 13:07:25 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,35 +11,32 @@ $(function($) {
         cancel : function () {
             console.log("cancel : ", this);
         },
-        'draw' : function () {
-
-        },
         format : function (value) {
-            if(value==1) {
-              return 'C#'
-            } else if(value==2) {
-              return 'D'
-            } else if(value==3) {
-              return 'D#'
-            } else if(value==4) {
-              return 'E'
-            } else if(value==5) {
-              return 'F'
-            } else if(value==6) {
-              return 'F#'
-            } else if(value==7) {
-              return 'G'
-            } else if(value==8) {
-              return 'G#'
-            } else if(value==9) {
-              return 'A'
-            } else if(value==10) {
-              return 'Bb'
-            } else if(value==11) {
-              return 'B'
-            } else {
-              return 'C'
-            }
+          if(value==1) {
+            return 'C#'
+          } else if(value==2) {
+            return 'D'
+          } else if(value==3) {
+            return 'D#'
+          } else if(value==4) {
+            return 'E'
+          } else if(value==5) {
+            return 'F'
+          } else if(value==6) {
+            return 'F#'
+          } else if(value==7) {
+            return 'G'
+          } else if(value==8) {
+            return 'G#'
+          } else if(value==9) {
+            return 'A'
+          } else if(value==10) {
+            return 'Bb'
+          } else if(value==11) {
+            return 'B'
+          } else {
+            return 'C'
+          }
         },
     });
 });
